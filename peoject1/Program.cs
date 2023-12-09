98508888c4ed5b13af5440705c21d6a773fbf4a5
Chhairak <chhairak11@gmail.com>
2023-12-09 15:50:30 +0700

    acb

---
+++ peoject1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+namespace peoject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float total = 15.50f;
+            char character = 'a';
+            string firstname = "Chhai";
+            bool isworking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(total);
+            Console.WriteLine(character);
+            Console.WriteLine(firstname);
+            Console.WriteLine(isworking);
+        }
+    }
+}
